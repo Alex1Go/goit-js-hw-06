a4e66e964072660644e4e6686744dce8799624d8
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-06-01 22:30:20 +0300

    add all

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+//const numberOfCategories = ();
+//numberOfCategories.forEach(function (number, index)) {
+ //   return number
+//}
+const list = document.querySelectorAll('.item')
+console.log(list.length)
+
